944fcadde14d65bf2201f61869857b63d8090986
ZhuNian <zhunianhhxy@163.com>
2015-09-16 14:10:40 +0800

    [JS] this in a function of a object

--- app/assets/javascripts/users_js.js
+++ app/assets/javascripts/users_js.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    this_in_js();
+    myJs = {};
+    myJs.name = 'this is a object';
+    myJs.func = this_in_js;
+    myJs.func();
 });
 function this_in_js(show_this){
     if (show_this == undefined){
