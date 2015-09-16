210f2f8a667c6a1f924769f1d723e30b5266638a
ZhuNian <zhunianhhxy@163.com>
2015-09-16 22:43:20 +0800

    [coffee] arrow -> is a normal function

--- app/assets/javascripts/users.coffee
+++ app/assets/javascripts/users.coffee
@@ -1,5 +1,13 @@
 class @User extends Widget
   buildDom: ->
-    @click_button = @element.find('#click_button')
+      @click_button = @element.find('#click_button')
   enhancePage: ->
+    @print_this();
+
   initialize: ->
+
+  print_this:(show_this) ->
+    if show_this == undefined
+      show_this = this;
+    console.log('this is this in function\n')
+    console.log(show_this)
--- app/assets/javascripts/users_js.js
+++ app/assets/javascripts/users_js.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
+});
+function click_button(){
     $('#click_button').click(function(){
         this_in_js();
         _this = this;
         this_in_js(_this);
     });
-});
+}
+
 function this_in_js(show_this){
-    if (show_this == undefined){
+    if (undefined == show_this) {
         show_this = this;
     }
     console.log('this is this in function\n')
