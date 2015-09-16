748395bed91018f96e17c5788ec9230fefaac93c
ZhuNian <zhunianhhxy@163.com>
2015-09-16 14:09:30 +0800

    [JS] this in normal function

--- app/assets/javascripts/users_js.js
+++ app/assets/javascripts/users_js.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
     this_in_js();
-
-    myJs = {};
-    myJs.name = 'this is a object';
-    myJs.func = this_in_js;
-    myJs.func();
 });
-function this_in_js(){
+function this_in_js(show_this){
+    if (show_this == undefined){
+        show_this = this;
+    }
     console.log('this is this in function\n')
-    console.log(this)
+    console.log(show_this)
 }
